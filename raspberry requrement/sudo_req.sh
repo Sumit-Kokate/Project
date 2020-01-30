713c455f2130a349d1c2b0d79680568c658c3cf9
Sumit-Kokate <sumitkokatesrk@gmail.com>
2020-01-30 10:32:32 +0530

    file

---
+++ raspberry requrement/sudo_req.sh
@@ -0,0 +1,10 @@
+sudo apt-get install pip=19.3.1=py36_1
+sudo apt-get install python=3.6.3=h3b118a2_4
+sudo apt-get install setuptools=42.0.2=py36_0
+sudo apt-get install vc=14.1=h0510ff6_4
+sudo apt-get install vs2015_runtime=14.16.27012=hf0eaf9b_0
+sudo apt-get install wheel=0.33.6=py36_0
+sudo apt-get install wincertstore=0.2=py36h7fe50ca_0
+
+//////////////////////////////////////////////////////
+sudo apt install python3-pip
